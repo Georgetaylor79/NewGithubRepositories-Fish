6e34d2d13b561cd6b61037b57bb4d0605862b7f9
George <george_rtaylor@yahoo.com>
2023-11-12 17:46:43 -0600

    commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// Imports
+const express = require('express')
+const app = expresss()
+const port = 3000
+
+//Listen on port 3000
+app.listen(port, () => console.info(`Listening on port ${port}`))
+
